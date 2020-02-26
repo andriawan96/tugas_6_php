4beb05b4cb61d6a27604a87faf5c58956f40d91c
andriawan <alhaqandriawan@gmail.com>
2020-02-26 10:41:17 +0700

    Tugas 6 php
    Tugas di kumpulkan untuk Niomic

---
+++ tugas6_1.php
@@ -0,0 +1,11 @@
+=============================
+<?php
+$habib = 21;
+$usia_aji = 4 + $habib;
+$usia_bintan = $usia_aji + 3;
+echo "<br>";
+echo "Usia Habib = $habib <br>";
+echo "Usia Aji = $usia_aji <br>";
+echo "Usia Bintan = $usia_bintan <br>";
+ ?>
+=============================
---
+++ tugas6_2.php
@@ -0,0 +1,13 @@
+=================================================
+<?php
+$sekilo = 15000;
+$sedus = 6*5;
+$hargadus= 2000*2;
+$total = $sekilo * $sedus + $hargadus ;
+echo "<br>";
+echo "Harga Jambu = Rp 15000 / Kg <br>";
+echo "Harga Kardus = Rp 2000 / pcs <br>";
+echo "Total Penjualan (Dus dan Jambu) = Rp $total <br>";
+
+ ?>
+ =================================================
